7b2e0391e4a18dd19767fb8170fea0784fba0432
Dongmin (Reo) Lee <reoim10@gmail.com>
2015-03-18 11:13:11 +1000

    Initial Commit

---
+++ ChildActivity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChildActivity
+//
+//  Created by Reo Lee on 18/03/2015.
+//  Copyright (c) 2015 One App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChildActivity/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChildActivity
+//
+//  Created by Reo Lee on 18/03/2015.
+//  Copyright (c) 2015 One App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
